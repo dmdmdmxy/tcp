65b04b9c08848d2c724c56a0f041b5c6bf0c035c
弗洛莉娅 <139844678+dmdmdmxy@users.noreply.github.com>
2025-01-23 14:03:20 +0800

    Update optimized_docker_timezone.sh

--- optimized_docker_timezone.sh
+++ optimized_docker_timezone.sh
@@ -20,38 +20,78 @@ echo "正在配置网络优化参数..."
 # 写入网络优化配置
 cat <<EOF >> /etc/sysctl.conf
 # 网络优化参数
-net.core.default_qdisc = fq
-net.ipv4.tcp_congestion_control = bbr
+net.ipv4.conf.all.route_localnet=1
+net.ipv4.ip_forward=1
+net.ipv4.conf.all.forwarding=1
+net.ipv4.conf.default.forwarding=1
+net.ipv6.conf.all.forwarding=1
+net.ipv6.conf.default.forwarding=1
+net.ipv6.conf.lo.forwarding=1
+net.bridge.bridge-nf-call-iptables=1
+net.netfilter.nf_conntrack_buckets=67108864
+net.netfilter.nf_conntrack_max=536870912
+net.netfilter.nf_conntrack_tcp_timeout_established=600
+net.netfilter.nf_conntrack_tcp_timeout_close_wait=10
+net.netfilter.nf_conntrack_tcp_timeout_fin_wait=30
+net.netfilter.nf_conntrack_tcp_timeout_time_wait=60
+net.netfilter.nf_conntrack_tcp_timeout_syn_recv=60
+net.netfilter.nf_conntrack_tcp_timeout_syn_sent=60
+net.netfilter.nf_conntrack_tcp_timeout_last_ack=30
+net.netfilter.nf_conntrack_tcp_timeout_unacknowledged=30
+net.netfilter.nf_conntrack_udp_timeout=5
+net.netfilter.nf_conntrack_udp_timeout_stream=5
+net.netfilter.nf_conntrack_generic_timeout=60
 
-net.ipv4.conf.all.rp_filter = 0
-net.ipv4.tcp_no_metrics_save = 1
-net.ipv4.tcp_ecn = 0
-net.ipv4.tcp_frto = 0
-net.ipv4.tcp_mtu_probing = 0
-net.ipv4.tcp_rfc1337 = 1
-net.ipv4.tcp_sack = 1
-net.ipv4.tcp_fack = 1
-net.ipv4.tcp_window_scaling = 1
-net.ipv4.tcp_adv_win_scale = 2
-net.ipv4.tcp_moderate_rcvbuf = 1
-net.ipv4.tcp_rmem = 4096 65536 16777216
-net.ipv4.tcp_wmem = 4096 65536 16777216
-net.core.rmem_max = 16777216
-net.core.wmem_max = 16777216
-net.ipv4.udp_rmem_min = 8192
-net.ipv4.udp_wmem_min = 8192
-net.ipv4.ip_local_port_range = 1024 65535
-net.ipv4.tcp_timestamps = 1
-net.ipv4.tcp_tw_reuse = 1
-net.ipv4.tcp_max_syn_backlog = 4096
-net.core.somaxconn = 4096
-net.ipv4.tcp_abort_on_overflow = 1
+net.ipv4.tcp_syncookies=1
+net.ipv4.tcp_retries1=3
+net.ipv4.tcp_retries2=5
+net.ipv4.tcp_orphan_retries=1
+net.ipv4.tcp_syn_retries=3
+net.ipv4.tcp_synack_retries=3
+net.ipv4.tcp_tw_reuse=1
+net.ipv4.tcp_fin_timeout=30
+net.ipv4.tcp_max_tw_buckets=65536
+net.ipv4.tcp_max_syn_backlog=65536
+net.core.netdev_max_backlog=65536
+net.core.somaxconn=65536
+net.ipv4.tcp_notsent_lowat=16384
+net.ipv4.tcp_keepalive_time=300
+net.ipv4.tcp_keepalive_probes=3
+net.ipv4.tcp_keepalive_intvl=60
 
-# 内存交换设置
-vm.swappiness = 10
+net.ipv4.tcp_fastopen=3
+net.ipv4.tcp_autocorking=0
+net.ipv4.tcp_slow_start_after_idle=0
+net.ipv4.tcp_no_metrics_save=1
+net.ipv4.tcp_ecn=0
+net.ipv4.tcp_frto=0
+net.ipv4.tcp_mtu_probing=0
+net.ipv4.tcp_sack=1
+net.ipv4.tcp_dsack=1
+net.ipv4.tcp_fack=1
+net.ipv4.tcp_window_scaling=1
+net.ipv4.tcp_adv_win_scale=1
+net.ipv4.tcp_moderate_rcvbuf=1
+net.ipv4.tcp_thin_linear_timeouts=1
+net.ipv4.tcp_rmem=4096 87380 67108864
+net.ipv4.tcp_wmem=4096 16384 67108864
+net.core.rmem_max=67108864
+net.core.wmem_max=67108864
+net.core.rmem_default=16384
+net.core.wmem_default=16384
+net.ipv4.udp_rmem_min = 16384
+net.ipv4.udp_wmem_min = 16384
+net.ipv4.tcp_mem=786432 1048576 26777216
+net.ipv4.udp_mem=65536 131072 262144
+net.ipv4.tcp_slow_start_after_idle=0
+net.ipv4.tcp_pacing_ss_ratio=1000
+net.ipv4.tcp_pacing_ca_ratio=200
 
-# 文件描述符限制
-fs.file-max = 6553560
+net.ipv4.tcp_congestion_control=bbr
+net.core.default_qdisc=fq
+net.ipv4.tcp_shrink_window=1
+net.ipv4.tcp_rfc1337=1
+
 EOF
 
 # 使配置生效
