c189697bf1de70512a1973e5f5cab1b9886188cf
弗洛莉娅 <139844678+dmdmdmxy@users.noreply.github.com>
2025-07-19 16:09:25 +0800

    Update sg3-2.sh

--- sg3-2.sh
+++ sg3-2.sh
@@ -280,63 +280,312 @@ else
     echo "目录 /etc/soga 已存在，跳过创建。"
 fi
 
-# 创建 /etc/soga/dns.yml 文件并写入内容
-cat <<EOF > /etc/soga/dns.yml
-gpt-jp01.ytjscloud.com:62580:
-  strategy: ipv4_first
-  rules:
-    - domain:openai.com
-    - domain:chat.openai.com
-    - domain:challenges.cloudflare.com
-    - domain:auth0.openai.com
-    - domain:platform.openai.com
-    - domain:ai.com
-    - domain:chatgpt.com
-    - domain:oaiusercontent.com
-    - domain:browser-intake-datadoghq.com
-    - domain:otokyo1a.turn.livekit.cloud
-    - domain:media.giphy.com
-    - domain:i1.wp.com
-    - domain:s.gravatar.com
-    - domain:api.revenuecat.com
-    - domain:auth0.com
-    - domain:o33249.ingest.sentry.io
-    - domain:oaistatic.com
-    - domain:featureassets.org
-    - domain:prodregistryv2.org
-EOF
-
-if [ $? -eq 0 ]; then
-    echo "文件 /etc/soga/dns.yml 已成功创建并写入内容。"
-else
-    echo "创建 /etc/soga/dns.yml 失败！"
-    exit 1
-fi
-
 # 创建 /etc/soga/routes.toml 文件并写入内容
 cat <<EOF > /etc/soga/routes.toml
-enable=true
+enable = true
 
 [[routes]]
-rules=[
-"geosite:netflix",
-"geosite:disney",
-"geosite:dazn",
+rules = [
+  # 流媒体
+  "geosite:netflix",
+  "geosite:disney",
+  "geosite:hulu",
+
+  "domain-suffix:bcbolthboa-a.akamaihd.net",
+  "domain-suffix:cf-images.ap-southeast-1.prod.boltdns.net",
+  "domain-suffix:cinemax.com",
+  "domain-suffix:dai3fd1oh325y.cloudfront.net",
+  "domain-suffix:execute-api.ap-southeast-1.amazonaws.com",
+  "domain-suffix:execute-api.us-east-1.amazonaws.com",
+  "domain-suffix:forthethrone.com",
+  "domain-suffix:hbo.com",
+  "domain-suffix:hbo.com.c.footprint.net",
+  "domain-suffix:hbo.com.edgesuite.net",
+  "domain-suffix:hbo.map.fastly.net",
+  "domain-suffix:hboasia.com",
+  "domain-suffix:hboasia1-i.akamaihd.net",
+  "domain-suffix:hboasia2-i.akamaihd.net",
+  "domain-suffix:hboasia3-i.akamaihd.net",
+  "domain-suffix:hboasia4-i.akamaihd.net",
+  "domain-suffix:hboasia5-i.akamaihd.net",
+  "domain-suffix:hboasialive.akamaized.net",
+  "domain-suffix:hbogeo.cust.footprint.net",
+  "domain-suffix:hbogo.co.th",
+  "domain-suffix:hbogo.com",
+  "domain-suffix:hbogo.eu",
+  "domain-suffix:hbogoasia.com",
+  "domain-suffix:hbogoasia.hk",
+  "domain-suffix:hbogoasia.id",
+  "domain-suffix:hbogoasia.ph",
+  "domain-suffix:hbogoasia.sg",
+  "domain-suffix:hbogoasia.tw",
+  "domain-suffix:hbogoprod-vod.akamaized.net",
+  "domain-suffix:hbolb.onwardsmg.com",
+  "domain-suffix:hbomax.com",
+  "domain-suffix:hbomaxcdn.com",
+  "domain-suffix:hbomaxdash.s.llnwi.net",
+  "domain-suffix:hbonow.com",
+  "domain-suffix:hbounify-prod.evergent.com",
+  "domain-suffix:manifest.prod.boltdns.net",
+  "domain-suffix:max.com",
+  "domain-suffix:maxgo.com",
+  "domain-suffix:now-ashare.com",
+  "domain-suffix:now-tv.com",
+  "domain-suffix:now.com",
+  "domain-suffix:now.com.hk",
+  "domain-suffix:nowe.com",
+  "domain-suffix:nowe.hk",
+  "domain-suffix:players.brightcove.net",
+  "domain-suffix:warnermediacdn.com",
+  "domain-suffix:youboranqs01.com",
+
+  # 学术资源
+  "domain:scholar.google.com",
+  "domain-suffix:1lib.cloud",
+  "domain-suffix:1lib.domains",
+  "domain-suffix:1lib.education",
+  "domain-suffix:1lib.eu",
+  "domain-suffix:1lib.limited",
+  "domain-suffix:1lib.pl",
+  "domain-suffix:1lib.to",
+  "domain-suffix:1lib.tw",
+  "domain-suffix:2lib.org",
+  "domain-suffix:3lib.net",
+  "domain-suffix:aclweb.org",
+  "domain-suffix:acm.org",
+  "domain-suffix:acs.org",
+  "domain-suffix:aiaa.org",
+  "domain-suffix:aip.org",
+  "domain-suffix:altmetric.com",
+  "domain-suffix:amamanualofstyle.com",
+  "domain-suffix:ams.org",
+  "domain-suffix:analytictech.com",
+  "domain-suffix:anb.org",
+  "domain-suffix:annualreviews.org",
+  "domain-suffix:apa.org",
+  "domain-suffix:apress.com",
+  "domain-suffix:aps.org",
+  "domain-suffix:art1lib.com",
+  "domain-suffix:arxiv.org",
+  "domain-suffix:ascelibrary.org",
+  "domain-suffix:asha.org",
+  "domain-suffix:asm.org",
+  "domain-suffix:asme.org",
+  "domain-suffix:astm.org",
+  "domain-suffix:b-ok.africa",
+  "domain-suffix:b-ok.asia",
+  "domain-suffix:b-ok.cc",
+  "domain-suffix:b-ok.global",
+  "domain-suffix:b-ok.org",
+  "domain-suffix:berkeley.edu",
+  "domain-suffix:biomedcentral.com",
+  "domain-suffix:biorxiv.org",
+  "domain-suffix:blackstonespoliceservice.com",
+  "domain-suffix:blackwell-synergy.com",
+  "domain-suffix:bmj.com",
+  "domain-suffix:book4you.org",
+  "domain-suffix:bookfi.net",
+  "domain-suffix:booksc.eu",
+  "domain-suffix:booksc.me",
+  "domain-suffix:booksc.org",
+  "domain-suffix:booksc.xyz",
+  "domain-suffix:bookshome.info",
+  "domain-suffix:bookshome.net",
+  "domain-suffix:bookshome.world",
+  "domain-suffix:brill.com",
+  "domain-suffix:cabdirect.org",
+  "domain-suffix:cambridge.org",
+  "domain-suffix:cambridgedigital.net",
+  "domain-suffix:cambridgemaths.org",
+  "domain-suffix:cambridgeschoolshakespeare.com",
+  "domain-suffix:cas.org",
+  "domain-suffix:cell.com",
+  "domain-suffix:clarivate.com",
+  "domain-suffix:cnki.net",
+  "domain-suffix:computer.org",
+  "domain-suffix:computingreviews.com",
+  "domain-suffix:cqvip.com",
+  "domain-suffix:crossref.org",
+  "domain-suffix:csiro.au",
+  "domain-suffix:de1lib.org",
+  "domain-suffix:deepdyve.com",
+  "domain-suffix:degruyter.com",
+  "domain-suffix:dentalhypotheses.com",
+  "domain-suffix:doi.info",
+  "domain-suffix:doi.org",
+  "domain-suffix:ebscohost.com",
+  "domain-suffix:elifesciences.org",
+  "domain-suffix:els-cdn.com",
+  "domain-suffix:elsevier-ae.com",
+  "domain-suffix:elsevier.com",
+  "domain-suffix:elsevier.io",
+  "domain-suffix:emerald.com",
+  "domain-suffix:endnote.com",
+  "domain-suffix:engineeringvillage.com",
+  "domain-suffix:epigeum.com",
+  "domain-suffix:europepmc.org",
+  "domain-suffix:evise.com",
+  "domain-suffix:frontiersin.org",
+  "domain-suffix:gale.com",
+  "domain-suffix:galegroup.com",
+  "domain-suffix:geoscienceworld.org",
+  "domain-suffix:ggsrv.com",
+  "domain-suffix:hindawi.com",
+  "domain-suffix:hk1lib.org",
+  "domain-suffix:ic.ac.uk",
+  "domain-suffix:icevirtuallibrary.com",
+  "domain-suffix:ieee.org",
+  "domain-suffix:ieeecomputer.org",
+  "domain-suffix:ieeecomputersociety.org",
+  "domain-suffix:imf.org",
+  "domain-suffix:imperial.ac.uk",
+  "domain-suffix:imperial.insendi.com",
+  "domain-suffix:imperialbusiness.school",
+  "domain-suffix:informs.org",
+  "domain-suffix:iop.org",
+  "domain-suffix:isca-speech.org",
+  "domain-suffix:isiknowledge.com",
+  "domain-suffix:jamanetwork.com",
+  "domain-suffix:japanknowledge.com",
+  "domain-suffix:jbc.org",
+  "domain-suffix:jbe-platform.com",
+  "domain-suffix:jhu.edu",
+  "domain-suffix:jmlr.org",
+  "domain-suffix:jneurosci.org",
+  "domain-suffix:jstor.org",
+  "domain-suffix:karger.com",
+  "domain-suffix:knovel.com",
+  "domain-suffix:kuke.com",
+  "domain-suffix:lawdata.com.tw",
+  "domain-suffix:libguides.com",
+  "domain-suffix:libsolutions.app",
+  "domain-suffix:libsolutions.domains",
+  "domain-suffix:libsolutions.net",
+  "domain-suffix:liebertpub.com",
+  "domain-suffix:literatumonline.com",
+  "domain-suffix:ma1lib.org",
+  "domain-suffix:madsrevolution.net",
+  "domain-suffix:mdpi.com",
+  "domain-suffix:mit",
+  "domain-suffix:mit.edu",
+  "domain-suffix:mit.net",
+  "domain-suffix:mitpressjournals.org",
+  "domain-suffix:mpg.de",
+  "domain-suffix:myilibrary.com",
+  "domain-suffix:nature.com",
+  "domain-suffix:ncbi.nlm.nih.gov",
+  "domain-suffix:nejm.org",
+  "domain-suffix:neurology.org",
+  "domain-suffix:newisiknowledge.com",
+  "domain-suffix:oecd-ilibrary.org",
+  "domain-suffix:oed.com",
+  "domain-suffix:omscr.com",
+  "domain-suffix:osapublishing.org",
+  "domain-suffix:oup.com",
+  "domain-suffix:ouplaw.com",
+  "domain-suffix:ovid.com",
+  "domain-suffix:ox.ac.uk",
+  "domain-suffix:oxfordaasc.com",
+  "domain-suffix:oxfordartonline.com",
+  "domain-suffix:oxfordbibliographies.com",
+  "domain-suffix:oxfordclinicalpsych.com",
+  "domain-suffix:oxforddnb.com",
+  "domain-suffix:oxfordfirstsource.com",
+  "domain-suffix:oxfordhandbooks.com",
+  "domain-suffix:oxfordlawtrove.com",
+  "domain-suffix:oxfordmedicine.com",
+  "domain-suffix:oxfordmusiconline.com",
+  "domain-suffix:oxfordpoliticstrove.com",
+  "domain-suffix:oxfordre.com",
+  "domain-suffix:oxfordreference.com",
+  "domain-suffix:oxfordscholarlyeditions.com",
+  "domain-suffix:oxfordscholarship.com",
+  "domain-suffix:oxfordwesternmusic.com",
+  "domain-suffix:peerj.com",
+  "domain-suffix:physiology.org",
+  "domain-suffix:pkulaw.com",
+  "domain-suffix:plos.org",
+  "domain-suffix:pnas.org",
+  "domain-suffix:princeton.edu",
+  "domain-suffix:proquest.com",
+  "domain-suffix:psyccareers.com",
+  "domain-suffix:readcube.com",
+  "domain-suffix:researchgate.net",
+  "domain-suffix:routledgehandbooks.com",
+  "domain-suffix:royalsocietypublishing.org",
+  "domain-suffix:rsc.org",
+  "domain-suffix:sagepub.com",
+  "domain-suffix:scholarpedia.org",
+  "domain-suffix:sci-hub.tw",
+  "domain-suffix:science.org",
+  "domain-suffix:sciencedirect.com",
+  "domain-suffix:sciencedirectassets.com",
+  "domain-suffix:sciencemag.org",
+  "domain-suffix:sciencenets.com",
+  "domain-suffix:scientificamerican.com",
+  "domain-suffix:scitation.org",
+  "domain-suffix:scopus.com",
+  "domain-suffix:semanticscholar.org",
+  "domain-suffix:serialssolutions.com",
+  "domain-suffix:sg1lib.org",
+  "domain-suffix:siam.org",
+  "domain-suffix:silverchair-cdn.com",
+  "domain-suffix:singlelogin.app",
+  "domain-suffix:singlelogin.me",
+  "domain-suffix:sipriyearbook.org",
+  "domain-suffix:spiedigitallibrary.org",
+  "domain-suffix:springer.com",
+  "domain-suffix:springerlink.com",
+  "domain-suffix:springernature.com",
+  "domain-suffix:statsmakemecry.com",
+  "domain-suffix:tandf.co.uk",
+  "domain-suffix:tandfonline.com",
+  "domain-suffix:taylorandfrancis.com",
+  "domain-suffix:taylorfrancis.com",
+  "domain-suffix:thelancet.com",
+  "domain-suffix:turnitin.com",
+  "domain-suffix:uchicago.edu",
+  "domain-suffix:ucla.edu",
+  "domain-suffix:ukwhoswho.com",
+  "domain-suffix:umass.edu",
+  "domain-suffix:un.org",
+  "domain-suffix:uni-bielefeld.de",
+  "domain-suffix:universitypressscholarship.com",
+  "domain-suffix:uq.edu.au",
+  "domain-suffix:uq.h5p.com",
+  "domain-suffix:veryshortintroductions.com",
+  "domain-suffix:wanfangdata.com",
+  "domain-suffix:wanfangdata.com.cn",
+  "domain-suffix:webofknowledge.com",
+  "domain-suffix:webofscience.com",
+  "domain-suffix:westlaw.com",
+  "domain-suffix:westlawchina.com",
+  "domain-suffix:wiley.com",
+  "domain-suffix:wkap.nl",
+  "domain-suffix:worldbank.org",
+  "domain-suffix:worldscientific.com",
+  "domain-suffix:yale.edu",
+  "domain-suffix:z-lib.org",
+  "domain-suffix:zlib.life",
+  "domain-suffix:zlibcdn.com",
+  "domain-suffix:zlibcdn2.com",
+  "domain-suffix:zotero.org"
 ]
+
 [[routes.Outs]]
-listen=""
-type="ss"
-server="nf-disney-sg.ytjscloud.com"
-port=4610
-password="D2AmLZIWlMulG5ki"
-cipher="aes-128-gcm"
+listen = ""
+type = "ss"
+server = "nf-disney-sg.ytjscloud.com"
+port = 4610
+password = "D2AmLZIWlMulG5ki"
+cipher = "aes-128-gcm"
 
 [[routes]]
-rules=["*"]
+rules = ["*"]
 
 [[routes.Outs]]
-listen=""
-type="direct"
+listen = ""
+type = "direct"
 EOF
 
 if [ $? -eq 0 ]; then
